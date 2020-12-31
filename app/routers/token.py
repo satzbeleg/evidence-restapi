095f1191f1528e9718c740d71da56a1e37ec93b7
UH <554c46@gmail.com>
2020-12-31 22:39:46 +0100

    fix #16

--- app/routers/token.py
+++ app/routers/token.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import secrets
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -14,9 +15,8 @@ router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-# TODO: Replace this with os.getenv and docker-compose settings!
 # How to create a SECRET_KEY: `openssl rand -hex 32`
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = secrets.token_hex(32)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
