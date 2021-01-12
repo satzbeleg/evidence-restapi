d18679dd9582dc9e8513f5cb986f0b92166c1b57
UH <554c46@gmail.com>
2021-01-12 16:56:30 +0100

    post api for random examples

--- app/routers/bestworst_random.py
+++ app/routers/bestworst_random.py
@@ -41,3 +41,25 @@ async def get_bestworst_random_exampleset(n_sentences: int,
             {"id": str(uuid.uuid4()), "text": lorem.sentence()}
             for _ in range(n_sentences)
         ]} for _ in range(n_examplesets)]
+
+
+# POST /bestworst/random/{n_sents}/{m_sets}
+# return M sets of N random sentences
+@router.post("/{n_sentences}/{n_examplesets}")
+async def get_bestworst_random_exampleset2(n_sentences: int,
+                                           n_examplesets: int,
+                                           params: dict):
+    if params:
+        keywords = params['lemmata']
+    else:
+        keywords = []
+        # keywords = lorem.sentence().split(" ")[0:random.randint(1,2)]
+    print(keywords)
+    
+    return [{
+        "set_id": str(uuid.uuid4()),
+        "lemmata": keywords,
+        "examples": [
+            {"id": str(uuid.uuid4()), "text": lorem.sentence()}
+            for _ in range(n_sentences)
+        ]} for _ in range(n_examplesets)]
